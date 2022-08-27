3e4d0e50c4f02bf646581c9e1d892cd692c2ef2e
Sandra <sandraliliana91@hotmail.com>
2022-08-26 20:33:31 -0500

    Funciones

---
+++ script_holamundo.js
@@ -0,0 +1,10 @@
+/*alert ("Hola") 
+ alert("Bienvenido") 
+        */
+let x=10
+let y=5
+
+function Funcion1(){document.getElementById("titulo grande").innerHTML = 'Reemplazo comentrario'}
+      
+function Suma(){
+document.getElementById("titulo grande").innerHTML = x+y}
